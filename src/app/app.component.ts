f646ff8b80d8dabb9f0bcd020f410d6a12a0409e
Raúl <rauulssanchezz@gmail.com>
2024-11-24 12:40:10 +0100

    Nuevo pryecto, header(no funcional) search-box(no funcional)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { SearchBoxComponent } from "./shared/search-box/search-box.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SearchBoxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
