5b531c985255b92437daf53ac8553a3c5fa5055a
Raúl <rauulssanchezz@gmail.com>
2024-11-25 16:24:40 +0100

    singup maquetacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProductsListComponent } from './shop/products-list/products-list.component';
 import { LoginComponent } from './auth/login/login.component';
+import { SingupComponent } from './auth/singup/singup.component';
 
 export const routes: Routes = [
 
-  { path:'', redirectTo: 'shop/products-list', pathMatch: 'full' },
+  { path: '', redirectTo: 'shop/products-list', pathMatch: 'full' },
   { path: 'shop/products-list', component: ProductsListComponent },
-  { path:'auth/login', component: LoginComponent }
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/singup', component: SingupComponent }
 
 ];
--- src/app/auth/singup/singup.component.ts
+++ src/app/auth/singup/singup.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-singup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './singup.component.html',
   styleUrl: './singup.component.css'
 })
 export class SingupComponent {
+
+  name: string = '';
+  gmail: string = '';
+  password: string = '';
+  postalCode: string = '';
+  country: string = '';
+  city: string = '';
+  street: string = '';
+  houseNumber: string = '';
+
+  saveFormValues() {
+    console.log({
+      name: this.name,
+      gmail: this.gmail,
+      password: this.password,
+      country: this.country,
+      city: this.city,
+      postalCode: this.postalCode,
+      street: this.street,
+      houseNumber: this.houseNumber
+    });
+  }
 
 }
